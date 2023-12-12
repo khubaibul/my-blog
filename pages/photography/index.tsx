facaa20f43b4f8902ebaf562a4f2fea0449eba3d
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-12 23:16:19 +0600

    Crop Imported

--- pages/photography/index.tsx
+++ pages/photography/index.tsx
@@ -4,6 +4,7 @@ import Footer from "../../components/Footer";
 import BAU from "../../public/images/BAU.png"
 import Cox from "../../public/images/Cox's_Bazar.jpg"
 import Crop_Field from "../../public/images/Crop_Field.jpg"
+import Fruits from "../../public/images/Fruits.jpg"
 
 const index = () => {
   return (
