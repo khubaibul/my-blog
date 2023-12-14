fdef4c56ac39c86f62fef6a6a3963ba3ee3474c6
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-14 22:05:17 +0600

    Hover Effect Implemented

--- pages/photography/index.tsx
+++ pages/photography/index.tsx
@@ -20,25 +20,19 @@ const index = () => {
       <div className="grid grid-cols-11 gap-8 px-10">
         <div className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-4">
           <figure>
-            <Image className="" src={BAU} loading={"eager"} alt="bannerImgOne" />
+            <Image
+              className=""
+              src={BAU}
+              loading={"eager"}
+              alt="bannerImgOne"
+            />
           </figure>
-          <div className=" absolute inset-0 pt-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100 flex justify-center items-end">
-            <div className="bg-red-600 w-full text-center text-slate-200">
-              <p>Khubaibul Islam</p>
-              {/* <p>
-              <FontAwesomeIcon
-                className="mr-1"
-                icon={faMoneyBill1}
-              ></FontAwesomeIcon>
-              BDT {course_price}
-            </p>
-            <p>
-              <FontAwesomeIcon
-                className="mr-1"
-                icon={faClock}
-              ></FontAwesomeIcon>
-              {course_duration}
-            </p> */}
+          <div className=" absolute inset-0 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100 flex justify-center items-center">
+            <div className="w-full text-center text-slate-200">
+              <h3>
+                Location: Bangladesh Agriculture University, Mymensingh,
+                Bangladesh
+              </h3>
             </div>
           </div>
         </div>
