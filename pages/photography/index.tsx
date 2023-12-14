09db790c2e9f52c76d357665c41f2b2c56a04add
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-14 22:02:27 +0600

    Image Edited With Hover Effect

--- pages/photography/index.tsx
+++ pages/photography/index.tsx
@@ -24,19 +24,66 @@ const index = () => {
           loading={"eager"}
           alt="bannerImgOne"
         />
-        <Image
-          className="col-span-3"
-          src={Cox}
-          loading={"eager"}
-          alt="bannerImgOne"
-        />
-        <Image
-          className="col-span-4"
-          src={Crop_Field}
-          loading={"eager"}
-          alt="bannerImgOne"
-        />
+        <div className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-4">
+        <figure>
+          <Image
+            className=""
+            src={Crop_Field}
+            loading={"eager"}
+            alt="bannerImgOne"
+          />
+        </figure>
+        <div className=" absolute inset-0 pt-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100 flex justify-center items-end">
+          <div className="bg-red-600 w-full text-center text-slate-200">
+            <p>Khubaibul Islam</p>
+            {/* <p>
+              <FontAwesomeIcon
+                className="mr-1"
+                icon={faMoneyBill1}
+              ></FontAwesomeIcon>
+              BDT {course_price}
+            </p>
+            <p>
+              <FontAwesomeIcon
+                className="mr-1"
+                icon={faClock}
+              ></FontAwesomeIcon>
+              {course_duration}
+            </p> */}
+          </div>
+        </div>
       </div>
+        <div className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-4">
+          <figure>
+            <Image
+              className=""
+              src={Crop_Field}
+              loading={"eager"}
+              alt="bannerImgOne"
+            />
+          </figure>
+          <div className=" absolute inset-0 pt-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100 flex justify-center items-end">
+            <div className="bg-red-600 w-full text-center text-slate-200">
+              <p>Khubaibul Islam</p>
+              {/* <p>
+              <FontAwesomeIcon
+                className="mr-1"
+                icon={faMoneyBill1}
+              ></FontAwesomeIcon>
+              BDT {course_price}
+            </p>
+            <p>
+              <FontAwesomeIcon
+                className="mr-1"
+                icon={faClock}
+              ></FontAwesomeIcon>
+              {course_duration}
+            </p> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
