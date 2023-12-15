9670f9e041158f2d5953bc1a662a8bb76480c7a5
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-15 18:50:23 +0600

    Added

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -9,7 +9,16 @@ const index = () => {
 
       <div className="min-h-screen bg-gray-800 py-6 flex flex-col justify-center sm:py-12">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div className="absolute inset-0 bg-gradient-to-r from-indigo-700 to-purple-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div className="text-white relative px-4 py-10 bg-indigo-400 shadow-lg sm:rounded-3xl sm:p-20">
+            <div className="text-center pb-6">
+              <h1 className="text-3xl">Contact Us!</h1>
 
+              <p className="text-gray-300">
+                Fill up the form below to send us a message.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
