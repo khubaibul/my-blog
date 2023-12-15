16759107497d7b408439a3f560ac18b8c594bd9c
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-15 18:57:52 +0600

    Form Added

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -7,10 +7,10 @@ const index = () => {
     <div>
       <Header />
 
-      <div className="min-h-screen bg-gray-800 py-6 flex flex-col justify-center sm:py-12">
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div className=" bg-bgColor flex flex-col justify-center sm:py-12">
+        <div className="relative py- sm:max-w-xl sm:mx-auto font-titleFont">
           <div className="absolute inset-0 bg-gradient-to-r from-indigo-700 to-purple-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="text-white relative px-4 py-10 bg-indigo-400 shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="text-white relative px-8 py-8 bg-indigo-400 shadow-lg sm:rounded-3xl">
             <div className="text-center pb-6">
               <h1 className="text-3xl">Contact Us!</h1>
 
