5c2440befc7324e9ca3ddaab2e138891de7d458b
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-15 18:53:26 +0600

    Form Added

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -18,6 +18,48 @@ const index = () => {
                 Fill up the form below to send us a message.
               </p>
             </div>
+
+            <form>
+              <input
+                className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Name"
+                name="name"
+              />
+
+              <input
+                className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                placeholder="Email"
+                name="email"
+              />
+
+              <input
+                className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Subject"
+                name="_subject"
+              />
+
+              <textarea
+                className="shadow mb-4 min-h-0 appearance-none border rounded h-64 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Type your message here..."
+                name="message"
+              />
+
+              <div className="flex justify-between">
+                <input
+                  className="shadow bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                  value="Send ➤"
+                />
+                <input
+                  className="shadow bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="reset"
+                />
+              </div>
+            </form>
           </div>
         </div>
       </div>
