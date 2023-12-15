2dc494fcd987d7abc28cc420615eaaa5d020ee7e
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-15 18:35:18 +0600

    Height Adjusted

--- pages/photography/index.tsx
+++ pages/photography/index.tsx
@@ -95,7 +95,12 @@ const index = () => {
             className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-4"
           >
             <figure>
-              <Image className="" src={Crop_Field} loading={"eager"} alt="bannerImgOne" />
+              <Image
+                className=""
+                src={Crop_Field}
+                loading={"eager"}
+                alt="bannerImgOne"
+              />
             </figure>
             <div className=" absolute inset-0 transition-opacity duration-200 bg-purple-600 bg-opacity-40 opacity-0 hover:opacity-100 flex justify-center items-center p-10">
               <div className="w-full text-center text-purple-600 bg-bgColor opacity-90 p-5">
@@ -121,14 +126,14 @@ const index = () => {
             </div>
           </Link>
         </div>
-        <div className="grid grid-cols-11 gap-8 px-10">
+        <div className="grid grid-cols-4 gap-8 px-10">
           <Link
             href={""}
-            className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-4"
+            className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-2"
           >
             <figure>
               <Image
-                className=""
+                className="h-[497px]"
                 src={Fruits}
                 loading={"eager"}
                 alt="bannerImgOne"
@@ -157,44 +162,10 @@ const index = () => {
               </div>
             </div>
           </Link>
+
           <Link
             href={""}
-            className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-3"
-          >
-            <figure>
-              <Image
-                className=""
-                src={Majar}
-                loading={"eager"}
-                alt="bannerImgOne"
-              />
-            </figure>
-            <div className=" absolute inset-0 transition-opacity duration-200 bg-purple-600 bg-opacity-40 opacity-0 hover:opacity-100 flex justify-center items-center p-10">
-              <div className="w-full text-center text-purple-600 bg-bgColor opacity-90 p-5">
-                <div className="flex justify-start items-center gap-0">
-                  <FaLocationArrow className="text-4xl" />
-                  <h3 className="font-titleFont text-lg font-semibold">
-                    Cox's Bazar Sea Beach, Cox's Bazar, Bangladesh
-                  </h3>
-                </div>
-                <div className="flex justify-start items-center gap-x-4">
-                  <FaCamera className="text-2xl" />
-                  <h3 className="font-titleFont text-lg font-semibold">
-                    Xiaomi Redmi7
-                  </h3>
-                </div>
-                <div className="flex justify-start items-center gap-x-4">
-                  <FaCalendar className="text-2xl" />
-                  <h3 className="font-titleFont text-lg font-semibold">
-                    17 August 2023
-                  </h3>
-                </div>
-              </div>
-            </div>
-          </Link>
-          <Link
-            href={""}
-            className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-4"
+            className="relative overflow-hidden transition duration-200 transform shadow-lg col-span-2"
           >
             <figure>
               <Image
