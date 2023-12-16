092a72d8ca53c91f6447d91e30f5c7cbd164939d
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-16 23:56:02 +0600

    Added

--- pages/photography/index.tsx
+++ pages/photography/index.tsx
@@ -18,7 +18,7 @@ const index = () => {
   return (
     <div>
       <Header />
-      <h1>My Clickz</h1>
+      <h1>My Clickz 📸</h1>
       <div className="flex flex-col gap-y-10 pt-10">
         <div className="grid grid-cols-11 gap-8 px-10">
           <Link
